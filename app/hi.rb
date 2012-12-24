b770713a0b636b9559b9576f0c95bac3a22ca887
Michael Makarov <info1117@gmail.com>
2012-12-24 12:26:28 -0800

    assets work

--- app/hi.rb
+++ app/hi.rb
@@ -3,6 +3,7 @@ class App < Sinatra::Base
 
   set :assets, Sprockets::Environment.new
 
+
 # Configure sprockets
   settings.assets.append_path "app/assets/javascripts"
   settings.assets.append_path "app/assets/stylesheets"
@@ -18,18 +19,20 @@ class App < Sinatra::Base
   ].inject([]) do |sum, v|
     sum + [File.join(v, 'javascripts'), File.join(v, 'stylesheets')]
   end
-  binding.pry
-  ([root] + ::Sinatra.rubygems_latest_specs.map(&:full_gem_path)).each do |root_path|
-    try_paths.map {|p| File.join(root_path, p) }.
-        select {|p| File.directory?(p) }.
-        each {|path| settings.assets.append_path(path) }
+  if ::Gem::Specification.respond_to? :latest_specs
+    gems = ::Gem::Specification.latest_specs
+  else
+    gems = ::Gem.source_index.latest_specs
+  end
+
+  ([root] + gems.map(&:full_gem_path)).each do |root_path|
+    try_paths.map {|p| File.join(root_path, p) }.select {|p| File.directory?(p) }.each {|path| settings.assets.append_path(path) }
   end
 
 
 
   get "/" do
     haml :index, :layout => :fuck
-    binding.pry
   end
 
   get "/javascripts/:file.js" do
@@ -40,7 +43,8 @@ class App < Sinatra::Base
   get "/stylesheets/:file.css" do
     content_type "text/css"
     settings.assets["#{params[:file]}.css"]
-    haml :index, :layout => :fuck
   end
+
+
 
 end
